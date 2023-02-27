a24b8f520078befb0732111ff7d46faff3777b3f
zimaster <symokevo@gmail.com>
2023-02-27 08:53:05 +0300

    Add more functionalities to main.js

--- js/main.js
+++ js/main.js
@@ -20,3 +20,42 @@ function loadBody () {
    
     document.getElementById("card-container").innerHTML = htmlText
   }
+
+  function handleSubmit() {
+    let question = document.getElementById("questionField").value;
+    let answer = document.getElementById("answerField").value;
+    var currentCards = localStorage.getItem("cards")
+    if (question && answer){
+      if (currentCards == null) {
+        currentCards = []
+      } else{
+        currentCards = JSON.parse(currentCards)
+      }
+      currentCards.push({question: question, answer: answer})
+      localStorage.setItem("cards", JSON.stringify(currentCards))
+      document.getElementById("questionField").value = ""
+      document.getElementById("answerField").value = ""
+      loadBody()
+    }
+  }
+   
+  function handleState (index) {
+    let card = document.getElementById(`card-${index}`)
+    if (card.style.display == "none") {
+      card.style.display = "block"
+    }else{
+      card.style.display = "none"
+    }
+  }
+   
+  function handleDelete (index) {
+    let currentCards = localStorage.getItem("cards")
+    if (currentCards == null) {
+      currentCards = []
+    }else{
+      currentCards = JSON.parse(currentCards)
+    }
+    currentCards.splice(index, 1)
+    localStorage.setItem("cards", JSON.stringify(currentCards))
+    loadBody()
+  }
